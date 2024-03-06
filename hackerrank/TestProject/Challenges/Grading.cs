1d68ab97120d36a2a0f6f7db7c243a8082a50fe1
Bair Dongak <bayer07b@gmail.com>
2024-03-06 02:06:07 -0800

    Grading

---
+++ hackerrank/TestProject/Challenges/Grading.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Challenges
+{
+    internal class Grading
+    {
+        public static List<int> GradingStudents(List<int> grades)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                int grade = grades[i];
+                if (grade >= 38)
+                {
+                    int dif = 5 - grade % 5;
+                    if (dif < 3)
+                    {
+                        grades[i] = grade + dif;
+                    }
+                }
+            }
+
+            return grades;
+        }
+
+        [TestCaseSource(nameof(Input))]
+        public static void StartTest(List<int> input, List<int> output)
+        {
+            var grades = GradingStudents(input);
+            Assert.That(grades, Is.EquivalentTo(output));
+        }
+
+        public static readonly object[] Input =
+        {
+            new object[]
+            {
+                new List<int>{ 73,67,38,33 },
+                new List<int>{ 75,67,40,33 }
+            }
+        };
+    }
+}
---
+++ hackerrank/TestProject/WarmUp/SalesByMarch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.WarmUp
+{
+    public class SalesByMarch
+    {
+        [TestCaseSource(nameof(Input))]
+
+        public static void SockMerchant(int n, List<int> ar, int expected)
+        {
+            int count = Count(n, ar);
+            Assert.That(expected, Is.EqualTo(count));
+        }
+
+        public static int Count(int n, List<int> ar)
+        {
+            var array = new int[101];
+            for (int i = 0; i < n; i++)
+            {
+                int x = ar[i];
+                array[x]++;
+            }
+
+            return array.Sum(x => x / 2);
+        }
+
+        public static readonly object[] Input =
+        {
+            new object[]
+            {
+                1,
+                new List<int>{ 100 },
+                0
+            }
+        };
+    }
+}
