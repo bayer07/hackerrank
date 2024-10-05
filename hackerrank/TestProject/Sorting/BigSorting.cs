bf074fbbe89c5e2bc6dfdbb8afc0fa0297bc1708
bayer07b@gmail.com <bayer07b@gmail.com>
2024-10-05 09:22:20 +0700

    Counting valleys

--- hackerrank/TestProject/Sorting/BigSorting.cs
+++ hackerrank/TestProject/Sorting/BigSorting.cs
@@ -1,33 +1,44 @@
-﻿namespace TestProject.Sorting
+﻿using System.Diagnostics;
+
+namespace TestProject.Sorting
 {
     internal class BigSorting
     {
         [Ignore("")]
         [TestCaseSource(nameof(Input))]
-        public void Sort(List<string> unsorted, List<string> expected)
+        public void Sort(List<string> array, List<string> expected)
         {
-            var sorted = Sort(unsorted);
-            Assert.That(sorted, Is.EqualTo(expected));
+            Sort(array);
+            Assert.That(array, Is.EqualTo(expected));
         }
 
-        public static List<string> Sort(List<string> unsorted)
+        [TestCaseSource(nameof(InputFromFile))]
+        public void TestFromFile(string[] input, string[] output)
         {
-            for (int i = 0; i < unsorted.Count; i++)
+            int n = int.Parse(input[0]);
+            var list = new List<string>(input);
+            list.RemoveAt(0);
+            Sort(list);
+            Assert.That(list.ToArray(), Is.EqualTo(output));
+        }
+
+        public static void Sort(List<string> array)
+        {
+            for (int i = 0; i < array.Count; i++)
             {
-                var num = double.Parse(unsorted[i]);
-                for (int j = i + 1; j < unsorted.Count; j++)
+                var num = ulong.Parse(array[i]);
+                for (int j = i + 1; j < array.Count; j++)
                 {
-                    var num2 = double.Parse(unsorted[j]);
+                    var num2 = ulong.Parse(array[j]);
                     if (num > num2)
                     {
-                        string numText = unsorted[i];
-                        unsorted[i] = unsorted[j];
-                        unsorted[j] = numText;
+                        string numText = array[i];
+                        array[i] = array[j];
+                        array[j] = numText;
                         num = num2;
                     }
                 }
             }
-            return unsorted;
         }
 
         public static readonly object[] Input =
@@ -38,5 +49,12 @@
                 new List<string>{ "1","3","3","5","10", "31415926535897932384626433832795" }
             }
         };
+
+        public static object[] InputFromFile()
+        {
+            var output = File.ReadAllLines($"{Directory.GetCurrentDirectory()}\\output02.txt");
+            var input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}\\input02.txt");
+            return new object[] { new object[2] { input, output } };
+        }
     }
 }
---
+++ hackerrank/TestProject/warmup/CountingValleys.cs
@@ -0,0 +1,54 @@
+﻿namespace TestProject.warmup
+{
+    internal class CountingValleys
+    {
+        public static int countingValleys(int steps, string path)
+        {
+            int valleys = 0, level = 0;
+            foreach (char c in path)
+            {
+                switch (c)
+                {
+                    case 'D':
+                        if (level == 0)
+                            valleys++;
+
+                        level--;
+                        break;
+                    case 'U':
+                        level++;
+                        break;
+                }
+            }
+
+            return valleys;
+        }
+
+        [TestCaseSource(nameof(Input))]
+        public void Sort(int steps, string path, int expected)
+        {
+            int actual = countingValleys(steps, path);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        public static readonly object[] Input =
+        {
+            new object[]
+            {
+                8, "DDUUUUDD", 1
+            },
+            new object[]
+            {
+                8, "UDDDUDUU", 1
+            },
+            new object[]
+            {
+                12, "DDUUDDUDUUUD", 2
+            },
+            new object[]
+            {
+                10, "UDUUUDUDDD", 0
+            }
+        };
+    }
+}
